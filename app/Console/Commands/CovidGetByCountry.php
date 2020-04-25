bfe54a574da50cb8a41844613c8ad6ad558be566
Dmitry Kuksin <frantomas6677@gmail.com>
2020-04-25 16:22:58 +0300

    Homework 12

--- app/Console/Commands/CovidGetByCountry.php
+++ app/Console/Commands/CovidGetByCountry.php
@@ -21,6 +21,7 @@ class CovidGetByCountry extends Command
         $this->covidStatService = $statService;
         parent::__construct();
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
